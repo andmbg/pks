18764e3acae1bd6b633eb7b7d96c3ffd2bfb4cc5
Andreas Domberg <domberg@posteo.de>
2024-01-01 16:12:29 +0100

    make import relative
    so it can perhaps run both standalone and in Flask

--- pks/src/visualization/visualize.py
+++ pks/src/visualization/visualize.py
@@ -9,7 +9,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-from pks.src.visualization.colormap import hsv_to_css, hsvtraj, max_nchildren
+from ...src.visualization.colormap import hsv_to_css, hsvtraj, max_nchildren
 
 
 logging.basicConfig(
