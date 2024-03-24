670373acd9ce346d58d8919674b5d4cd7513edfe
Andreas Domberg <andom@posteo.de>
2024-03-24 20:14:09 +0100

    rename endpoint to route

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -1,5 +1,5 @@
 metadata = {
     "synopsis": "The *Polizeiliche Kriminalstatistik (PKS)* is a statistical system used by the police in Germany to collect and analyze data on criminal offenses. Its main function is to provide a comprehensive overview of crime trends and patterns in the country. The PKS collects data on various types of crimes, including theft, assault, drug offenses, and more. It also includes information on the demographics of offenders and victims, as well as details about the circumstances surrounding the crimes. The data collected through the PKS is used by law enforcement agencies, policymakers, and researchers to assess the effectiveness of crime prevention measures, allocate resources, and develop strategies to combat crime. The PKS plays a crucial role in understanding the nature and extent of crime in Germany and informing evidence-based decision-making in the field of criminal justice.",
     "title": "Polizeiliche Kriminalstatistik (PKS)",
-    "endpoint": "/pks/",
+    "route": "/pks/",
 }
