3368f8deada3422f0d0f3d705f63ca7a26df3a16
Andreas Domberg <andom@posteo.de>
2024-03-30 11:10:03 +0100

    move metadata to root

---
+++ __init__.py
@@ -0,0 +1,6 @@
+metadata = {
+    "title": "Polizeiliche Kriminalstatistik (PKS)",
+    "route": "/pks/",
+    "thumbnail": "preview.png",
+    "synopsis": "The *Polizeiliche Kriminalstatistik (PKS)* is a statistical system used by the police in Germany to collect and analyze data on criminal offenses. Its main function is to provide a comprehensive overview of crime trends and patterns in the country. The PKS collects data on various types of crimes, including theft, assault, drug offenses, and more. It also includes information on the demographics of offenders and victims, as well as details about the circumstances surrounding the crimes. The data collected through the PKS is used by law enforcement agencies, policymakers, and researchers to assess the effectiveness of crime prevention measures, allocate resources, and develop strategies to combat crime. The PKS plays a crucial role in understanding the nature and extent of crime in Germany and informing evidence-based decision-making in the field of criminal justice.",
+}
