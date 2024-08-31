e1501d1d1551e3ce96e774cec99ee2cbab7d38fa
Andreas Domberg <andom@posteo.de>
2024-08-31 10:01:57 +0200

    adapt init to bilingual routes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 metadata = {
     "title": "Crime Statistics from the German Police",
-    "route": "/pks/",
-    "thumbnail": "dashapps/pks/preview.png",
+    "route": "/en/pks/",
+    "thumbnail": "dashapps/en/pks/preview.png",
     "synopsis": "Die Polizeiliche Kriminalstatistik (PKS) kommuniziert jährlich eine Aufstellung der von den unterschiedlichen Polizeibehörden erhobenen Delikte. Das System hat seine Schwächen, da keine wissenschaftliche (oder wenigstens außerpolizeiliche) Kontrolle über die Erhebungsmethoden besteht. Allerdings lassen sich mit Vorsicht einige Phänomene hier mit Zahlen hinterlegen, etwa die sich ändernde Gesetzgebung zu Widerstandshandlungen gegen Polizeibeamte."
 }
