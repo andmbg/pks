a1ddccb1a686bd4bebb625afb69aa3aa1954581d
Andreas Domberg <andom@posteo.de>
2024-09-14 09:26:28 +0200

    i18ize metadata

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
 metadata = {
-    "title": "Polizeiliche Kriminalstatistik (PKS)",
-    "route": "/pks/",
-    "thumbnail": "dashapps/pks/preview.png",
-    "synopsis": "Die Polizeiliche Kriminalstatistik (PKS) kommuniziert jährlich eine Aufstellung der von den unterschiedlichen Polizeibehörden erhobenen Delikte. Das System hat seine Schwächen, da keine wissenschaftliche (oder wenigstens außerpolizeiliche) Kontrolle über die Erhebungsmethoden besteht. Allerdings lassen sich mit Vorsicht einige Phänomene hier mit Zahlen hinterlegen, etwa die sich ändernde Gesetzgebung zu Widerstandshandlungen gegen Polizeibeamte."
+    "de": {
+        "title": "Polizeiliche Kriminalstatistik (PKS)",
+        "route": "/de/pks/",
+        "thumbnail": "dashapps/de/pks/preview.png",
+        "synopsis": "Die Polizeiliche Kriminalstatistik (PKS) kommuniziert jährlich eine Aufstellung der von den unterschiedlichen Polizeibehörden erhobenen Delikte. Das System hat seine Schwächen, da keine wissenschaftliche (oder wenigstens außerpolizeiliche) Kontrolle über die Erhebungsmethoden besteht. Allerdings lassen sich mit Vorsicht einige Phänomene hier mit Zahlen hinterlegen, etwa die sich ändernde Gesetzgebung zu Widerstandshandlungen gegen Polizeibeamte."
+    },
+    "en": {
+        "title": "Police Crime Statistics (PKS)",
+        "route": "/en/pks/",
+        "thumbnail": "dashapps/en/pks/preview.png",
+        "synopsis": "On a yearly basis, the official crime statistics (PKS) publishes figures on a range of offences registered by the different German police bodies. The system has some drawbacks on account of there being no scientific (or at least civil) control over the data collection methods. However, with this aspect in mind, and with a measure of caution, some observations can be enriched with numbers here, for instance effects from changes in laws around resistance against police officers."
+    },
 }
